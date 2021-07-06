4f0d9913ea3cd0566445a70f96b6d7fb86e3e3da
irinadeeva <dee.irina.a@mail.com>
2021-07-06 13:04:04 +0300

    day05

---
+++ src/day05/ex00/Chat/src/main/java/edu/school21/chat/app/Main.java
@@ -0,0 +1,4 @@
+package day05.ex00.Chat.src.main.java.edu.school21.chat.app;
+
+public class Main {
+}
---
+++ src/day05/ex00/Chat/src/main/java/edu/school21/chat/logic/User.java
@@ -0,0 +1,5 @@
+package day05.ex00.Chat.src.main.java.edu.school21.chat.logic;
+
+public class User {
+
+}
