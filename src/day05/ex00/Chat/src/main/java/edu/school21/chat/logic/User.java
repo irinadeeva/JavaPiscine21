a8c2555120bca03ffe99d392e9e907de9bb21f16
irinadeeva <dee.irina.a@mail.com>
2021-07-06 15:14:50 +0300

    day05

--- src/day05/ex00/Chat/src/main/java/edu/school21/chat/logic/User.java
+++ src/day05/ex00/Chat/src/main/java/edu/school21/chat/logic/User.java
@@ -1,9 +1,11 @@
 package edu.school21.chat.logic;
 
+import java.util.List;
+
 public class User {
     long userID;
     String login;
     String password;
     List <Chatroom> createdRooms;
-    List <Chatroom> where a user socializes
+    List<Chatroom> whereausersocializes;
 }
