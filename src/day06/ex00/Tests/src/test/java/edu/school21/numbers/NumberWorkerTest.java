459a10a7e98145422d3881a8129f54fde2efbc98
irinadeeva <dee.irina.a@mail.com>
2021-07-07 13:42:35 +0300

    day06 ex00

--- src/day06/ex00/Tests/src/test/java/edu/school21/numbers/NumberWorkerTest.java
+++ src/day06/ex00/Tests/src/test/java/edu/school21/numbers/NumberWorkerTest.java
@@ -1,6 +1,5 @@
 package edu.school21.numbers;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.ValueSource;
